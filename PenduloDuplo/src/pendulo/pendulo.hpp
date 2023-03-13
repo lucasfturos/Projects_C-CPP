c66d6f46583084dd907e0cb6c4943be89e45b888
lucasfturos <lucasfturos@gmail.com>
2023-03-13 16:06:08 -0300

    Pendulo Duplo

--- PenduloDuplo/src/pendulo/pendulo.hpp
+++ PenduloDuplo/src/pendulo/pendulo.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
-#include <vector>
-#include <cstdlib>
 #include <iostream>
+#include <vector>
 
 #include "../plot/plot.hpp"
 #include "../timer/timer.hpp"
@@ -16,22 +15,20 @@ private:
 
   int x1{}, x2{};
   int y1{}, y2{};
-  
+
   float accumulator{0.0F};
   float frameStart{0.0F};
   float currentTime{0.0F};
 
   float alpha1{0.0F}, alpha2{0.0F};
-  float 
-      l1{120.0F}, l2{120.0F},   // tamanho da corda
+  float l1{120.0F}, l2{120.0F}, // tamanho da corda
       m1{10.0F}, m2{10.0F},     // massa da esfera na extremidade
       O1{2.0F * pi / 2.0F},     // theta O1 e O2 do angulo
-      O2{2.0F * pi / 3.0F}, 
-      w1{0.0F}, w2{0.0F};       // velocidade angular
+      O2{2.0F * pi / 3.0F}, w1{0.0F}, w2{0.0F}; // velocidade angular
 
   std::vector<std::vector<int>> trace;
   char canvas[HEIGHT / dH][WIDTH / dW + 1];
-  
+
   void formulation();
 
 public:
