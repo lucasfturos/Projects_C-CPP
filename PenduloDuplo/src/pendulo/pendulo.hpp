6cb3270efc467983eca9adf0cd1981088c228db1
lucasfturos <lucasfturos@gmail.com>
2023-03-22 14:15:08 -0300

    Organização do repositório

--- PenduloDuplo/src/pendulo/pendulo.hpp
+++ PenduloDuplo/src/pendulo/pendulo.hpp
@@ -21,10 +21,12 @@ class Pendulo : public Timer, public Plot {
     float currentTime{0.0F};
 
     float alpha1{0.0F}, alpha2{0.0F};
-    float l1{120.0F}, l2{120.0F}, // tamanho da corda
+    float 
+        l1{120.0F}, l2{120.0F},   // tamanho da corda
         m1{10.0F}, m2{10.0F},     // massa da esfera na extremidade
+        w1{0.0F}, w2{0.0F},       // velocidade angular
         O1{2.0F * pi / 2.0F},     // theta O1 e O2 do angulo
-        O2{2.0F * pi / 3.0F}, w1{0.0F}, w2{0.0F}; // velocidade angular
+        O2{2.0F * pi / 3.0F};
 
     std::vector<std::vector<int>> trace;
     char canvas[col][line + 1];
