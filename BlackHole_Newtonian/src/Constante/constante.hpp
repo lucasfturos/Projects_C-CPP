7b38a5e2bc0d32ac62806448e1d5da58752cb903
lucasfturos <lucasfturos@gmail.com>
2023-04-03 14:23:13 -0300

    PenduloDuplo - SFML - Ajuste

--- BlackHole_Newtonian/src/Constante/constante.hpp
+++ BlackHole_Newtonian/src/Constante/constante.hpp
@@ -1,5 +1,9 @@
+#pragma once
+
 namespace Constantes {
 constexpr int width{1300}, height{1080};
 constexpr float fps{300.0f};
 constexpr float pi{3.14159265358979323846F};
+constexpr float M_SOL{1.989e+30}; // Massa do Sol em quilograma
+constexpr float mass_bh{M_SOL * 10};
 } // namespace Constantes
