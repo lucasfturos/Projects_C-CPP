7d4742b2d838c27066f027e4f46a747e04d37ae1
lucasfturos <lucasfturos@gmail.com>
2023-04-12 17:51:37 -0300

    Separando os objetos

--- BlackHole_Newtonian/src/BlackHole/black_hole.cpp
+++ BlackHole_Newtonian/src/BlackHole/black_hole.cpp
@@ -5,31 +5,32 @@ BlackHole::BlackHole(float half_width, float half_height) {
     this->half_height = half_height;
 }
 auto BlackHole::setupObjectBlackHole() -> void {
-    black_hole_radius.setRadius(radius * 0.009f);
+    black_hole_radius.setRadius(radius * err_show);
     black_hole_radius.setOrigin(black_hole_radius.getRadius(),
                                 black_hole_radius.getRadius());
     black_hole_radius.setFillColor(sf::Color::Black);
     black_hole_radius.setPosition(half_width, half_height);
-    black_hole_radius.setPointCount(1000);
+    black_hole_radius.setPointCount(point_count);
 }
 
 auto BlackHole::setupObjectAtratorRegion() -> void {
-    region1_atrator.setRadius((radius * 0.009f) * 3 + 32);
+    region1_atrator.setRadius((radius * err_show) * 3 + 32);
     region1_atrator.setOrigin(region1_atrator.getRadius(),
                               region1_atrator.getRadius());
     region1_atrator.setOutlineColor(sf::Color::Black);
     region1_atrator.setPosition(half_width, half_height);
     region1_atrator.setOutlineThickness(2.0f);
-    region1_atrator.setPointCount(1000);
+    region1_atrator.setPointCount(point_count);
 
-    region2_atrator.setRadius((radius * 0.009f) * 3 + 16);
+    region2_atrator.setRadius((radius * err_show) * 3 + 16);
     region2_atrator.setOrigin(region2_atrator.getRadius(),
                               region2_atrator.getRadius());
     region2_atrator.setOutlineColor(sf::Color::Black);
     region2_atrator.setPosition(half_width, half_height);
     region2_atrator.setOutlineThickness(2.0f);
-    region2_atrator.setPointCount(1000);
+    region2_atrator.setPointCount(point_count);
 }
+
 auto BlackHole::render() -> void {
     texture.create(width, height);
     this->setTexture(texture.getTexture());
--- BlackHole_Newtonian/src/BlackHole/black_hole.hpp
+++ BlackHole_Newtonian/src/BlackHole/black_hole.hpp
@@ -6,17 +6,21 @@
 #include <vector>
 
 #include <Constante/constante.hpp>
+#include <Photon/photon.hpp>
 
 using namespace Constantes;
 
 class BlackHole : public sf::Sprite {
   private:
+    int point_count{1000};
+    float err_show{0.009f};
     float half_width{}, half_height{};
 
+    std::shared_ptr<Photon> photon;
     sf::RenderTexture texture;
-    sf::CircleShape black_hole_radius;
     sf::CircleShape region1_atrator;
     sf::CircleShape region2_atrator;
+    sf::CircleShape black_hole_radius;
 
   public:
     void setupObjectBlackHole();
